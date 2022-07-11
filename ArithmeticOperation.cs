37721a4922c129be6c270e65171e3406bf0ca6d2
Jhon Mark Acopiado <jacopiado@fullscale.io>
2022-07-11 08:20:22 +0800

    new class

---
+++ ArithmeticOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practice
+{
+    public class ArithmeticOperation
+    {
+        public virtual int Operation(int x, int b)
+        {
+            Console.WriteLine("adding", x, b);
+            return x + b;
+        }
+    }
+    class Addition: ArithmeticOperation
+    {
+        public override int Operation(int x, int b)
+        {
+            return x + b;
+        }
+    }
+    class Substraction: ArithmeticOperation
+    {
+        public override int Operation(int x, int b)
+        {
+            return x - b;
+        }
+    }
+
+}
